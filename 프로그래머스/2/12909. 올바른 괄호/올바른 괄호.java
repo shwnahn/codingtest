822be658a4be6369e6f91c2c87e5ed635e8c52be
SIHWAN AHN <131660478+shwnahn@users.noreply.github.com>
2025-02-15 12:55:28 +0900

    [level 2] Title: 올바른 괄호, Time: 6.23 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -16,9 +16,6 @@ class Solution {
         }
         if (opened != 0) return false;
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
         return answer;
     }
 }
